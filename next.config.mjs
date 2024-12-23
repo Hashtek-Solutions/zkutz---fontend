e3520137b1f656b41409edbca90371145d939ec4
Sandip Sarkar <sarkarsandip7910@gmail.com>
2024-12-23 15:59:53 +0530

    yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
     domains: ["images.unsplash.com"],
   },
-  output: 'export',
 };
 
 export default nextConfig;
