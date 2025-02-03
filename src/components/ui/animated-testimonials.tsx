aa3844ae8ccad8e75db0e90eccd79df508956a82
Sandip Sarkar <sarkarsandip7910@gmail.com>
2025-02-03 14:52:59 +0530

    export

--- src/components/ui/animated-testimonials.tsx
+++ src/components/ui/animated-testimonials.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import React from "react"
 import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react";
 import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
